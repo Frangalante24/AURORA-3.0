beb58abf1604699a8d15de2c0458f8e53f09b33e
antonionunes12 <44400100+antonionunes12@users.noreply.github.com>
2021-05-22 17:22:25 +0100

    Update Aurora.h

--- MAIN/Aurora.h
+++ MAIN/Aurora.h
@@ -28,7 +28,7 @@
 #define Minimo_acc 2.0                                                              //g's
 #define Minimo_temp 300.0                                                           // milisegundos
 //#define it_v_ejecao 3.0                                                           //apagar??
-#define tempo_v_ejecao 300.0                                                        //tempo de amostragem velocidade para ejeção
+#define tempo_v_ejecao 300.0 //depende muito da freq, 3Hz impossibilita 300ms       //tempo de amostragem velocidade para ejeção
 #define tempo_h_ejecao 1500.0                                                       //tempo de amostragem altitude máxima para ejeção
 #define tempo_seguranca 10000.0                                                     //milisegundos
 #define period 26800.0                                                              // Periodo de Ejeção em Milisegundos
